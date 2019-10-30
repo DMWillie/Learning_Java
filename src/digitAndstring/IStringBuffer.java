d9976abf56a8d61bc77f293feeef9be960f7a164
Willie <GMWillie@gmail.com>
2019-10-30 23:33:14 +0800

    Init Commit

---
+++ src/digitAndstring/IStringBuffer.java
@@ -0,0 +1,17 @@
+package digitAndstring;
+
+/*  Author: 北辰
+    日期: 30/10/2019
+    功能: IStringBuffer接口
+ */
+
+public interface IStringBuffer {
+    public void append(String str);             //追加字符串
+    public void append(char c);                 //追加字符
+    public void insert(int pos,char b);         //指定位置插入字符
+    public void insert(int pos,String b);       //指定位置插入字符串
+    public void delete(int start);              //从开始位置删除剩下的
+    public void delete(int start,int end);      //从开始位置删除结束位置-1
+    public void reverse();                      //反转
+    public int length();                        //返回长度
+}
---
+++ src/digitAndstring/MyStringBuffer.java
@@ -0,0 +1,70 @@
+package digitAndstring;
+
+/*  Author: 北辰
+    日期: 30/10/2019
+    功能: MyStringBuffer实现IStringBuffer接口
+ */
+
+public class MyStringBuffer implements IStringBuffer {
+    int capacity = 16;
+    int length = 0;
+    char[] value;
+    //无参构造方法
+    public MyStringBuffer() {
+        value = new char[capacity];
+    }
+    //有参构造方法
+    public MyStringBuffer(String str){
+        /**方法一
+        if(null!=str){
+            value = str.toCharArray();
+            length = value.length;
+
+            if(capacity<length){
+                capacity = length*2;
+            }
+        }**/
+        //方法二
+        this();
+        if(null==str)
+            return;
+        if(capacity<str.length()){
+            capacity = str.length()*2;
+            value = new char[capacity];
+        }
+        if(capacity>=str.length()){
+            //str.copyValueOf(value,0,str.length());
+            System.arraycopy(str.toCharArray(),0,value,0,str.length());
+        }
+        length = str.length();
+    }
+    @Override
+    public void append(String str) {
+        // TODO Auto-generated method stub
+    }
+    @Override
+    public void append(char c) {
+        // TODO Auto-generated method stub
+    }
+    @Override
+    public void insert(int pos, char b) {
+        // TODO Auto-generated method stub
+    }
+    @Override
+    public void delete(int start) {
+        // TODO Auto-generated method stub
+    }
+    @Override
+    public void delete(int start, int end) {
+        // TODO Auto-generated method stub
+    }
+    @Override
+    public void reverse() {
+        // TODO Auto-generated method stub
+    }
+    @Override
+    public int length(){
+        // TODO Auto-generated method stub
+        return 0;
+    }
+}
