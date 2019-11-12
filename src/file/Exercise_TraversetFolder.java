9423716fc3d4948e7a2627bf1561abf31fbc76a4
Willie <GMWillie@gmail.com>
2019-11-12 23:19:23 +0800

    增加遍历子文件夹的方法，并将之前的遍历文件封装成方法

--- src/file/Exercise_TraversetFolder.java
+++ src/file/Exercise_TraversetFolder.java
@@ -9,11 +9,21 @@ package file;
 import java.io.File;
 
 public class Exercise_TraversetFolder {
+    private static long maxLen = 0,minLen = Integer.MAX_VALUE;
+    private static String maxLenFile = "",minLenFile="";
     public static void main(String[] args){
-        File f = new File("c:/Windows");
-        long maxLen = 0,minLen = Integer.MAX_VALUE;
-        String maxLenFile = "",minLenFile="";
-        File[] fs = f.listFiles();
+        File f = new File("d:/DATA");
+        traverseSubFile(f);
+        //traverseSubFolder(f);
+        System.out.println(f+"目录下(不包含子文件夹):");
+        //System.out.println(f+"目录下(包含子文件夹):");
+        System.out.println("最大的文件是: "+maxLenFile+" 其大小为: "+maxLen+"字节");
+        System.out.println("最小的文件是: "+minLenFile+" 其大小为: "+minLen+"字节");
+    }
+
+    //获取当前目录下最大和最小的文件(不包含子文件夹)
+    public static void traverseSubFile(File f){
+        File[] fs = f.listFiles();      //将f下的子文件和子文件夹以文件列表形式返回
         for(int i=0;i<fs.length;i++){
             if(fs[i].isDirectory())     //排除子文件夹
                 continue;
@@ -26,8 +36,24 @@ public class Exercise_TraversetFolder {
                 minLenFile = fs[i].toString();
             }
         }
-        System.out.println(f+"目录下:");
-        System.out.println("最大的文件是: "+maxLenFile+" 其大小为: "+maxLen+"字节");
-        System.out.println("最小的文件是: "+minLenFile+" 其大小为: "+minLen+"字节");
+    }
+
+    //获取当前目录下最大和最小的文件(包含子文件夹)
+    public static void traverseSubFolder(File f){
+        File[] fs = f.listFiles();      //将f下的子文件和子文件夹以文件列表形式返回
+        for(int i=0;i<fs.length;i++){
+            if(fs[i].isDirectory())     //如果是目录则递归调用
+                traverseSubFolder(fs[i]);
+            else {
+                if (fs[i].length() > maxLen) {
+                    maxLen = fs[i].length();
+                    maxLenFile = fs[i].toString();
+                }
+                if (fs[i].length() < minLen) {
+                    minLen = fs[i].length();
+                    minLenFile = fs[i].toString();
+                }
+            }
+        }
     }
 }
