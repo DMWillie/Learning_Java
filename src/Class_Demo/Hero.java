43d3cde73db969460195008ea56824a99de7b2d6
Willie <GMWillie@gmail.com>
2019-11-24 12:15:41 +0800

    重写构造方法,增加英雄序号,并重写toString()方法

--- src/Class_Demo/Hero.java
+++ src/Class_Demo/Hero.java
@@ -16,13 +16,21 @@ public class Hero implements Serializable {
     public float hp; //血量
     float armor; //护甲
     int moveSpeed; //移动速度
+    public int num;     //每个英雄的序号
 
+    public Hero(int num){
+        this.num = num;
+    }
+    @Override
+    public String toString(){
+        return "hero:"+this.num;
+    }
     public static void main(String[] args) {
         //创建一个对象
-        new Hero();
+        new Hero(0);
         //使用一个引用来指向这个对象
-        Hero h1 = new Hero();
-        Hero h2 = new Hero();
+        Hero h1 = new Hero(1);
+        Hero h2 = new Hero(2);
         Hero h3;
         h3 = h1;
         System.out.println(h1);
@@ -31,13 +39,13 @@ public class Hero implements Serializable {
         //h3和h1指向同一个对象,和h2指向的对象不同,因此打印的结果h1和h3相同,而和h2不同
 
         //创建两个英雄,并设置它们的属性
-        Hero garen =  new Hero();
+        Hero garen =  new Hero(21);
         garen.name = "盖伦";
         garen.hp = 616.28f;
         garen.armor = 27.536f;
         garen.moveSpeed = 350;
 
-        Hero teemo =  new Hero();
+        Hero teemo =  new Hero(23);
         teemo.name = "提莫";
         teemo.hp = 383f;
         teemo.armor = 14f;
