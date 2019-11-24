98a346c03b9d78f6f740e23371e855a9919a9a38
Willie <GMWillie@gmail.com>
2019-11-24 16:45:47 +0800

    Init Commit

---
+++ src/data/Fish.java
@@ -0,0 +1,13 @@
+package data;
+public class Fish {
+    public String color;
+    public Fish() {
+    }
+    public void setColor(String  color){
+        this.color = color;
+    }
+
+    public String  getColor(){
+        return this.color;
+    }
+}
