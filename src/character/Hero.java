1fef0d8d765c913213295b44a6f2c64f8d4b795d
Willie <GMWillie@gmail.com>
2019-12-12 00:18:09 +0800

    增加英雄的序号属性

--- src/character/Hero.java
+++ src/character/Hero.java
@@ -2,13 +2,14 @@ package character;
 
 /*  Author: 北辰
     日期: 24/11/2019
+    修改日期: 12/12/2019,每个英雄新增一个序号的属性
     功能: 重写toString()方法的Hero类
  */
 
 public class Hero {
     public String name;
     public float hp;
-
+    public int num;         //英雄的序号
     public int damage;
 
     public Hero(){
