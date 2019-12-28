40eb19edc94465ef015486bd1305a4d4f39b7ae0
Willie <GMWillie@gmail.com>
2019-12-28 17:51:18 +0800

    Init Commit

---
+++ src/collection/IHashMap.java
@@ -0,0 +1,11 @@
+package collection;
+
+/*  Author: 北辰
+    日期: 28/12/2019
+    功能: 接口IHashMap,有抽象方法put和get
+ */
+public interface IHashMap{
+    public void put(String key,Object object);
+    public Object get(String key);
+}
+
