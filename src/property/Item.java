75da41f7ade7cb8167080838eb0f5d3930d40a91
Willie <GMWillie@gmail.com>
2020-01-01 22:40:40 +0800

    重写toString()方法

--- src/property/Item.java
+++ src/property/Item.java
@@ -2,6 +2,7 @@ package property;
 
 /*  Author: 北辰
     日期: 30/11/2019
+    修改日期: 01/01/2020 ,重写toString()方法
     功能: 物品类Item
  */
 
@@ -17,5 +18,8 @@ public class Item {
     }
     public void effect(){
         System.out.println("物品使用后，可以有效果");
+    }
+    public String toString(){
+        return "[Item "+name+", price="+price+"]";
     }
 }
