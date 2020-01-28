f18ac2b54d95361c7cffc7bb597798306e82eac4
Willie <GMWillie@gmail.com>
2020-01-28 21:30:57 +0800

    解决只能发送消息却接收不到消息的bug

--- src/socket/Exercise_ChatClient.java
+++ src/socket/Exercise_ChatClient.java
@@ -11,23 +11,81 @@ import java.util.Scanner;
 
 public class Exercise_ChatClient {
     public static void main(String[] args) throws IOException {
-        //连接到本机的8888端口(服务器)
-        Socket s= new Socket("127.0.0.1",8888);
-        System.out.println(s);
-        //打开字节输入流
-        InputStream is = s.getInputStream();
-        //把输入流封装在DataInputStream
+        Socket client=new Socket("127.0.0.1",8888);
+        System.out.println("试图与服务器 127.0.0.1的端口8888建立连接");
+        System.out.println("已与服务器建立连接"+client);
+        //创建输入流,输出流
+        OutputStream os=client.getOutputStream();
+        InputStream is=client.getInputStream();
+        DataOutputStream dos = new DataOutputStream(os);
         DataInputStream dis = new DataInputStream(is);
-        //使用Scanner读取控制台的输入，并发送到服务端
-        Scanner sc = new Scanner(System.in);
-        StringBuffer sendMsg = null;
-        while(String.valueOf(sendMsg)!=null || dis.readUTF()!=null){
-            if(dis.readUTF()!=null){
-                System.out.println("收到服务端信息:\t"+dis.readUTF());
+        Scanner sc=new Scanner(System.in);
+        while(true) {
+            /*
+             * 客户端先发送消息
+             */
+//            dos = new DataOutputStream(os);
+            System.out.print("客户端发送: ");
+            String sendMsg=sc.nextLine();
+            dos.writeUTF(sendMsg);
+            //退出循环的条件,客户端发送"bye"
+            if("bye".equals(sendMsg)) {
+                System.out.println("客户端断开连接");
+                break;
             }
-            sendMsg = new StringBuffer(sc.nextLine());
+            /*
+             * 客户端接收消息
+             */
+//            dis = new DataInputStream(is);
+            String receiveMsg = dis.readUTF();
+            System.out.println("收到服务端消息: "+receiveMsg);
+            if("bye".equals(receiveMsg)) {
+                System.out.println("服务端已断开连接!!!");
+                break;
+            }
         }
+        //关闭连接
+        sc.close();
         dis.close();
-        s.close();
+        dos.close();
+        client.close();
+        /**以下代码客户端也接收不到消息,因为客户端和服务端代码的顺序都是先接收后发送,这就造成一直在等待对方发送*/
+//        //连接到本机的8888端口(服务器)
+//        Socket s= new Socket("127.0.0.1",8888);
+//        System.out.println(s);
+//        //打开字节输出流
+//        OutputStream os = s.getOutputStream();
+//        //把输出流封装在DataOutputStream
+//        DataOutputStream dos = new DataOutputStream(os);
+//        //打开字节输入流
+//        InputStream is = s.getInputStream();
+//        //把输入流封装在DataInputStream
+//        DataInputStream dis = new DataInputStream(is);
+//        //使用Scanner读取控制台的输入，并发送到服务端
+//        Scanner sc = new Scanner(System.in);
+//        while(true) {
+//            //接收消息
+//            dis = new DataInputStream(is);
+//            String receiveMsg = dis.readUTF();
+//            if (receiveMsg!=null)
+//                System.out.println("收到服务端信息:\t" + receiveMsg);
+//            else
+//                break;
+//            //发送消息
+//            String sendMsg = sc.nextLine();
+//            dos = new DataOutputStream(os);
+//            dos.writeUTF(sendMsg);
+//        }
+        /**以下代码都客户端都接收不到信息,只能发送*/
+//        while(sendMsg!= || receiveMsg!=null){
+//            if(dis.readUTF()!=null){
+//                System.out.println("收到服务端信息:\t"+dis.readUTF());
+//            }
+//            sendMsg = new String(sc.nextLine());
+//            dos.writeUTF(sendMsg);
+//        }
+//        dos.close();
+//        dis.close();
+//        s.close();
     }
 }
--- src/socket/Exercise_ChatServer.java
+++ src/socket/Exercise_ChatServer.java
@@ -12,27 +12,87 @@ import java.util.Scanner;
 
 public class Exercise_ChatServer {
     public static void main(String[] args) throws IOException {
-        //服务端打开端口8888
-        ServerSocket ss = new ServerSocket(8888);
-        //在8888端口上打开监听,看是否有连接请求过来
-        System.out.println("监听在端口号:8888");
-        Socket s = ss.accept();
-        System.out.println(s);
-        //打开字节输入流
-        InputStream is = s.getInputStream();
-        //把输入流封装在DataInputStream
+        ServerSocket server=new ServerSocket(8888);
+        System.out.println("正在监听端口8888");
+        Socket socket=server.accept();
+        System.out.println("接收到连接请求 "+socket);
+        //创建输入输出流
+        OutputStream os = socket.getOutputStream();
+        InputStream is = socket.getInputStream();
+        DataOutputStream dos = new DataOutputStream(os);
         DataInputStream dis = new DataInputStream(is);
-        //使用Scanner读取控制台的输入，并发送到客户端
-        Scanner sc = new Scanner(System.in);
-        StringBuffer sendMsg = null;
-        while(String.valueOf(sendMsg)!=null ||dis.readUTF()!=null){
-            if(dis.readUTF()!=null){
-                System.out.println("收到客户端信息:\t"+dis.readUTF());
+        Scanner sc=new Scanner(System.in);
+        while(true) {
+            /*
+             * 接收消息
+             */
+//            dis = new DataInputStream(is);
+            String receiveMsg = dis.readUTF();
+            System.out.println("收到客户端消息: "+receiveMsg);
+            if("bye".equals(receiveMsg)) {
+                System.out.println("客户端已断开连接!!!");
+                break;
             }
-            sendMsg = new StringBuffer(sc.nextLine());
+
+            /*
+             * 发送消息
+             */
+//            dos = new DataOutputStream(os);
+            System.out.print("服务端发送消息: ");
+            String sendMsg = sc.nextLine();
+            dos.writeUTF(sendMsg);
+            if("bye".equals(sendMsg)) {
+                System.out.println("服务端断开连接");
+                break;
+            }
         }
+        sc.close();
         dis.close();
-        s.close();
-        ss.close();
+        dos.close();
+        socket.close();
+        server.close();
+        /**以下代码服务端收不到任何消息,只能发送消息,因为客户端与服务端的代码顺序一致,都是先接收消息*/
+//        //服务端打开端口8888
+//        ServerSocket ss = new ServerSocket(8888);
+//        //在8888端口上打开监听,看是否有连接请求过来
+//        System.out.println("监听在端口号:8888");
+//        Socket s = ss.accept();
+//        System.out.println(s);
+//        //打开字节输入流
+//        InputStream is = s.getInputStream();
+//        //把输入流封装在DataInputStream
+//        DataInputStream dis = new DataInputStream(is);
+//        //打开字节输出流
+//        OutputStream os = s.getOutputStream();
+//        //把输出流封装在DataOutputStream
+//        DataOutputStream dos = new DataOutputStream(os);
+//        //使用Scanner读取控制台的输入，并发送到客户端
+//        Scanner sc = new Scanner(System.in);
+//        while(true) {
+//            //接收消息
+//            dis = new DataInputStream(is);
+//            String receiveMsg = dis.readUTF();
+//            if (receiveMsg!=null)
+//                System.out.println("收到客户端信息:\t" + receiveMsg);
+//            else
+//                break;
+//            //发送消息
+//            String sendMsg = sc.nextLine();
+//            dos = new DataOutputStream(os);
+//            dos.writeUTF(sendMsg);
+//        }
+        /**以下代码服务端收不到任何消息,只能发送消息*/
+//        System.out.println(dis.readUTF());
+//        while(sendMsg!=null || dis.readUTF()!=null){
+//            if(dis.readUTF()!=null){
+//                System.out.println("收到客户端信息:\t"+dis.readUTF());
+//            }
+//            sendMsg = new String(sc.nextLine());
+//            dos.writeUTF(sendMsg);
+//        }
+//        dos.close();
+//        dis.close();
+//        s.close();
+//        ss.close();
     }
 }
