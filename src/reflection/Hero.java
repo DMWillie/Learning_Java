2a3a3e1f1cbecc33740dbb92e2707c9fdcccf61a
Willie <GMWillie@gmail.com>
2020-01-29 15:45:14 +0800

    增加setter和getter方法,以及其他的实例方法

--- src/reflection/Hero.java
+++ src/reflection/Hero.java
@@ -18,4 +18,55 @@ public class Hero {
         System.out.println("初始化 copyright");
         copyright = "版权由Riot Games公司所有";
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getHp() {
+        return hp;
+    }
+
+    public void setHp(float hp) {
+        this.hp = hp;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Hero(){
+    }
+    public Hero(String name){
+        this.name = name;
+    }
+    @Override
+    public String toString(){
+        return "Hero [name="+name+"]";
+    }
+
+    public boolean isDead(){
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public void attackHero(Hero h2){
+        System.out.println(this.name+ " 正在攻击 " + h2.getName());
+    }
 }
